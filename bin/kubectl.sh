b18f33f611ba2e21fc4b0389f7bc303d45e3d89d
Gary Morse <gmorse81@gmail.com>
2018-02-14 16:52:12 -0500

    update.

--- bin/kubectl.sh
+++ bin/kubectl.sh
@@ -5,6 +5,6 @@ echo ${PLUGIN_KEYFILE} > /keyfile.json
 gcloud auth activate-service-account --key-file=/keyfile.json --project=${PLUGIN_PROJECT}
 gcloud container clusters get-credentials ${PLUGIN_CLUSTER} --zone ${PLUGIN_ZONE} --project ${PLUGIN_PROJECT}
 
-python /run_commands.py \'${PLUGIN_KUBECTL_COMMANDS}\' | /bin/bash
+python /run_commands.py
 
 exit $?
--- python/run_commands.py
+++ python/run_commands.py
@@ -1,7 +1,22 @@
-import sys, json, subprocess;
+import os, subprocess, string;
 
-commands = json.loads(sys.argv[1])
+environment_var = os.getenv('PLUGIN_KUBECTL_COMMANDS', '')
+print os.getenv('PLUGIN_KUBECTL_COMMANDS', '')
 
-for command in commands:
-  print('kubectl ' + str(command))
+if environment_var:
+  commands = string.split(environment_var, ",")
 
+  for command in commands:
+    try:
+      p = subprocess.Popen('kubectl ' + str(command), shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+      (out,err) = p.communicate()
+      if p.returncode == 0:
+        print ("command kubectl %s succeeded, returned: %s" % (str(command),str(out)))
+      elif p.returncode <= 125:
+        print ("command kubectl %s failed, exit-code=%d error = %s" % (str(command), p.returncode, str(err)))
+      elif p.returncode == 127:
+        print ("Not found")
+      else:
+        sys.exit("'%s' likely crashed, shell retruned code %d" % (cmd,e.returncode))
+    except OSError as e:
+      print("Execution failed:", e)
